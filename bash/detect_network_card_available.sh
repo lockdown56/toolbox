2f51d10abc6ac1df9c23991752d492b68b8e4817
lockdown56 <lockdown56@163.com>
2020-05-18 18:40:05 +0800

    support args

--- bash/detect_network_card_available.sh
+++ bash/detect_network_card_available.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+url=$1
+if [[ -z url ]]
+then
+    url="baidu.com"
+fi
+
 interfaces=$(ifconfig | grep mtu | awk -F ':' '{print $1}')
 
 for interface in ${interfaces[@]}
 do
-  ping -I $interface -W 1 -c1 badu.com &> /dev/null
+  ping -I $interface -W 1 -c1 $url &> /dev/null
   if [[ "$?" == 0 ]]
   then
     echo "$interface is ok"
