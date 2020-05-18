e235c66b613fabfc50907832e12829a78f62d509
lockdown56 <lockdown56@163.com>
2020-05-18 18:11:08 +0800

    detect which network card is available

---
+++ bash/detect_network_card_available.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+interfaces=$(ifconfig | grep mtu | awk -F ':' '{print $1}')
+
+for interface in ${interfaces[@]}
+do
+  ping -I $interface -W 1 -c1 badu.com &> /dev/null
+  if [[ "$?" == 0 ]]
+  then
+    echo "$interface is ok"
+  else
+    echo "$interface cannot use"
+  fi
+done
+
